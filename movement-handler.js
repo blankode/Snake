9a74ebe4c01ff4ac53613c8afbb6279f6c99665f
blankode <porosnicu.bogdan@outlook.com>
2021-12-02 21:34:48 +0100

    made one major function for movement

--- movement-handler.js
+++ movement-handler.js
@@ -4,113 +4,64 @@ let repeatMove = null,
 
 //avoid collision
 let currentDirection = null,
-    previousDirection = null;
+    previousDirection = 'right';
 
 //listen to keyboard inputs so we can move the snake
 document.addEventListener('keydown', function (event) {
-    if (score != null) {
-        clearInterval(repeatMove);
+    //starts movement repetition
+    if (currentDirection === null) {
+        repeatMove = setInterval(moveSnake, speed);
+    }
         if ((event.key === 'A' || event.key === 'a' || event.key === 'ArrowLeft') && previousDirection !== 'right') {
             currentDirection = 'left';
-            repeatMove = setInterval(moveLeft, speed);
         } else if ((event.key === 'D' || event.key === 'd' || event.key === 'ArrowRight') && previousDirection !== 'left') {
             currentDirection = 'right';
-            repeatMove = setInterval(moveRight, speed);
         } else if ((event.key === 'S' || event.key === 's' || event.key === 'ArrowDown') && previousDirection !== 'up') {
             currentDirection = 'down';
-            repeatMove = setInterval(moveDown, speed);
         } else if ((event.key === 'W' || event.key === 'w' || event.key === 'ArrowUp') && previousDirection !== 'down') {
             currentDirection = 'up';
-            repeatMove = setInterval(moveUp, speed);
         } else {
             if (previousDirection === 'left') {
-                repeatMove = setInterval(moveLeft, speed);
+                currentDirection = 'left';
             } else if (previousDirection === 'right') {
-                repeatMove = setInterval(moveRight, speed);
+                currentDirection = 'right';
             } else if (previousDirection === 'down') {
-                repeatMove = setInterval(moveDown, speed);
+                currentDirection = 'down';
             } else if (previousDirection === 'up') {
-                repeatMove = setInterval(moveUp, speed);
+                currentDirection = 'up';
             }
         }
         previousDirection = currentDirection;
-    }
 });
 
 //snake movement
-function moveLeft() {
-    if (headY <= -1) {
+function moveSnake() {
+    if (headY <= -1 || headX <= -1 || headY >= tableSize || headX >= tableSize) {
         gameOver('wall');
     }
     refreshHeadCoordinates();
-    --headY;
+    if (currentDirection === 'left') {
+        --headY;
+    } else if (currentDirection === 'right') {
+        ++headY;
+    } else if (currentDirection === 'down') {
+        ++headX;
+    } else if (currentDirection === 'up') {
+        --headX;
+    }
     snake.shift();
     snake.push(headX + `,` + headY);
     if (document.getElementById(headX + `,` + headY).classList.contains('food')) {
         refreshTailCoordinates();
-        --tailY;
-        snake.push(tailX + `,` + tailY);
-        ++score;
-        generateFood();
-    } else if (document.getElementById(headX + `,` + headY).classList.contains('body') || document.getElementById(headX + `,` + headY).classList.contains('tail')) {
-        gameOver('body');
-    } else if (headY <= -1) {
-        gameOver('wall');
-    }
-    refreshSnake();
-}
-
-function moveRight() {
-    if (headY >= tableSize) {
-        gameOver('wall');
-    }
-    refreshHeadCoordinates();
-    ++headY;
-    snake.shift();
-    snake.push(headX + `,` + headY);
-    if (document.getElementById(headX + `,` + headY).classList.contains('food')) {
-        refreshTailCoordinates();
-        ++tailY;
-        snake.push(tailX + `,` + tailY);
-        ++score;
-        generateFood();
-    } else if (document.getElementById(headX + `,` + headY).classList.contains('body') || document.getElementById(headX + `,` + headY).classList.contains('tail')) {
-        gameOver('body');
-    }
-    refreshSnake();
-}
-
-function moveDown() {
-    if (headX >= tableSize) {
-        gameOver('wall');
-    }
-    refreshHeadCoordinates();
-    ++headX;
-    snake.shift();
-    snake.push(headX + `,` + headY);
-    if (document.getElementById(headX + `,` + headY).classList.contains('food')) {
-        refreshTailCoordinates();
-        ++tailX;
-        snake.push(tailX + `,` + tailY);
-        ++score;
-        generateFood();
-    } else if (document.getElementById(headX + `,` + headY).classList.contains('body') || document.getElementById(headX + `,` + headY).classList.contains('tail')) {
-        gameOver('body');
-    }
-    refreshSnake();
-}
-
-function moveUp() {
-    if (headX <= -1) {
-        gameOver('wall');
-    }
-    refreshHeadCoordinates();
-    --headX;
-    snake.shift();
-    snake.push(headX + `,` + headY);
-    if (document.getElementById(headX + `,` + headY).classList.contains('food')) {
-        refreshTailCoordinates();
-        --tailX;
+        if (currentDirection === 'left') {
+            --tailY;
+        } else if (currentDirection === 'right') {
+            ++tailY;
+        } else if (currentDirection === 'down') {
+            ++tailX;
+        } else if (currentDirection === 'up') {
+            --tailX;
+        }
         snake.push(tailX + `,` + tailY);
         ++score;
         generateFood();
